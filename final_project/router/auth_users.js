9a0f37a193689cc71a5c9d85351875c681bdfcae
sunilKmishra18 <sunilmishra18.creativity@gmail.com>
2024-07-27 06:02:20 -0400

    Changes updated

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -5,69 +5,81 @@ const regd_users = express.Router();
 
 let users = [];
 
-const isValid = (username)=>{ //returns boolean
-    let userExist = users.filter((user) => {
-        return user.username === username;
-      });
-      if (userExist.length > 0) {
+const isValid = (username) => {
+    let userswithsamename = users.filter((user) => {
+        return user.username === username
+    });
+    if (userswithsamename.length > 0) {
         return true;
-      } else {
+    } else {
         return false;
-      }
+    }
 }
 
-const authenticatedUser = (username,password)=>{ //returns boolean
+const authenticatedUser = (username, password) => {
     let validusers = users.filter((user) => {
-        return user.username === username && user.password === password;
-      });
-      if (validusers.length > 0) {
+        return (user.username === username && user.password === password)
+    });
+    if (validusers.length > 0) {
         return true;
-      } else {
+    } else {
         return false;
-      }
+    }
 }
 
-//only registered users can login
-regd_users.post("/login", (req,res) => {
+// Task 7: Logging in as a registered user
+regd_users.post("/login", (req, res) => {
     const username = req.body.username;
     const password = req.body.password;
     if (!username || !password) {
-      return res.status(404).json({ message: "Error logging in" });
+        return res.status(404).json({ message: "Error logging in, please try again." });
     }
     if (authenticatedUser(username, password)) {
-      let accessToken = jwt.sign(
-        {
-          data: password,
-        },
-        "access",
-        { expiresIn: 60 * 60 }
-      );
-  
-      req.session.authorization = {
-        accessToken,
-        username,
-      };
-      return res.status(200).send("Customer successfully logged in");
+        let accessToken = jwt.sign({
+            data: password
+        }, 'access', { expiresIn: 60*60 });
+
+        req.session.authorization = {
+            accessToken, username
+        }
+           
+        //return res.status(200).send(`Customer successfully logged in!`);
+        return res.status(200).json({
+            error: false,
+            accessToken,
+            message: "Customer successfully logged in!",
+        });
     } else {
-      return res
-        .status(208)
-        .json({ message: "Invalid Login. Check username and password" });
+        return res.status(208).json({ message: "Invalid Login, please check username and password and try again." });
     }
 });
 
-// Add a book review
+// Task 8: Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
-  let username = req.session.authorization["username"];
-  let isbn = req.params.isbn;
-  let findUser = users[username];
-  let findBook = books[isbn];
-  if (findUser) {
-    findBook["review"] = req.body.review;
-    res.send(`Book with the isbn  ${isbn} updated.`);
-  } else {
-    findBook.push({ review: review });
-    res.send(`Book with the isbn new ${isbn} updated.`);
-  }
+    const isbn = req.params.isbn;
+    const username = req.session.authorization.username;
+    const accessToken = req.session.authorization.accessToken;
+    const review = req.body.review;
+
+    if (books[isbn]){
+        books[isbn].reviews[username] = review;
+        res.send(`The review of the book with ISBN ${isbn} from user ${username} has been added/updated.`);
+    } else {
+        res.send(`No books with ISBN ${isbn} were found in the database.`);
+    }
+});
+
+// Task 9: Delete a book review
+regd_users.delete("/auth/review/:isbn", (req, res) => {
+    const isbn = req.params.isbn;
+    const username = req.session.authorization.username;
+    
+    if (books[isbn].reviews[username]){
+        delete books[isbn].reviews[username];
+        res.send(`The review of the book with ISBN ${isbn} from user ${username} has been deleted.`);
+    } else {
+        res.send(`No reviews with ISBN ${isbn} from user ${username} were found in the database.`);
+    }
 });
 
 module.exports.authenticated = regd_users;
